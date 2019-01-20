6635d6085a203403fb77962d60a20aa0c8eee8dc
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-01-20 22:24:18 +0000

    17-01-2019

--- js/webconf.js
+++ js/webconf.js
@@ -2,14 +2,15 @@ window.onload = function(){
 
    
 
-const url_base = "https://fcawebbook.herokuapp.com"
+const urlBase = "https://fcawebbook.herokuapp.com";
 
-const btnLogin = document.getElementById("btnLogin")
+const btnLogin = document.getElementById("btnLogin");
+
+const btnRegister = document.getElementById("btnRegister");
 
 
 
-
-
+/*----------------inscriçao admin-------------*/
 btnLogin.addEventListener("click", () => {
     swal({
         title:"Login de admin",
@@ -23,7 +24,7 @@ btnLogin.addEventListener("click", () => {
         preConfirm:() =>{
             const name = document.getElementById('txtEmail').value
             const email = document.getElementById('txtPass').value
-            return fetch ('${urlbase}/signin',{
+            return fetch ('${urlBase}/signin',{
                 headers:{
                     "Content-Type": "application/x-www-form-urlencoded"
                 },
@@ -41,7 +42,7 @@ btnLogin.addEventListener("click", () => {
             });
         }
 
-    }).then (result => {
+    }) .then (result => {
         if(result.value) {
         if(!result.value.err_code) {
         swal ({title: "Login sucesso!"})
@@ -55,8 +56,8 @@ btnLogin.addEventListener("click", () => {
 
 
 
-const btnRegister = document.getElementById("btnRegister")
-/*increver na webconference*/
+
+/*--------------------increver na webconference--------------------------*/
 btnRegister.addEventListener("click", function() {
 swal({
      title:"Inscrição na WebConference",
@@ -71,7 +72,7 @@ swal({
         const name = document.getElementById('txtName').value
         const email = document.getElementById('txtEmail').value       
         
-             return fetch('$ {url_base}/conferences/1/participants/${email}', {
+             return fetch('$ {urlBase}/conferences/1/participants/${email}', {
                  headers: { "Content-Type": "application/x-www-form-urlencoded"},
                  method: "POST",
                  body: 'nomeparticipant=${name}'
@@ -104,17 +105,17 @@ swal({
 (async ()=> {
     const renderSpeakers = document.getElementById("renderSpeakers")
     let txtSpeakers = ""
-    const response = await fetch('${urlBase}/conferences/1/speakers')
+    const response = await fetch(`${urlBase}/conferences/1/speakers`)
     const speakers = await response.json()
 
     for (const speaker of speakers) {
-        txtSpeakers += " ";
-        <div class ="col-sm-4">
+        txtSpeakers += `
+        <div class="col-sm-4">
           <div class ="team-member">
             <img id="${speaker.idSpeaker}" class="mx-auto rounded-circle viewSpeaker" src="${speaker.foto}" alt="">
             <h4>${speaker.nome}</h4>
             <p class="text-muted"> ${speaker.cargo}</p>
-            <ul class="list-inline social-buttons">
+            <ul class="list-inline social-buttons">`
         if(speaker.twitter!==null) {
             txtSpeakers += '<li class="list-inline-item"><a href="${speaker.twitter}" target="_blank"><i class="fab fa-twitter"></i></a> </li> '
         }
@@ -125,15 +126,15 @@ swal({
             txtSpeakers += '<li class= "list-inline-item" ><a href="${speaker.linkedin}" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>'
         }
         txtSpeakers += `
-        </ul>
-        </img>
+            </ul>
+            </img>
+         </div>
         </div>
-    </div>
-     
+        `
     }
     renderSpeakers.innerHTML = txtSpeakers
 
-        /*gerir click na imagem para exibiçao da janela modal*/
+        /*-----------------gerir click na imagem para exibiçao da janela modal------------------*/
         const btnView = document.getElementsByClassName("viewSpeaker")
         for(let i = o;i< btnView.lenght;i++){
             btnView[i].addEventListener("click", () =>{
@@ -152,8 +153,8 @@ swal({
                 }
             })
         }
-
     }) ();
+    
 
 /*-----------------------Get sponsor from server-------------------------*/
 (async () => {
@@ -195,7 +196,7 @@ contactForm.addEventListener ("submit",async function () {
         swall ('Mensagem não Enviada', result.value.message.pt, 'error')
     }
 });
-}
+
     
 
 
@@ -214,7 +215,7 @@ const mapProp = {
 /*-------mapa--------*/
 const map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-/*--janela de informaçao--*/
+/*------janela de informaçao-------*/
 const infowindow = new google.maps.InfoWindow ({
     content:"É aqui a WebConference!"
 })
@@ -235,4 +236,4 @@ marker.addListener('click', function(){
 
 
 
-
+}
